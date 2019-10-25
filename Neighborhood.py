ed6d5f4e3416cd794e19b106f2bf7398693b71ed
travisrase <travisrase@Traviss-MacBook-Pro.local>
2019-10-25 13:26:47 -0400

    added neighborhood.py file

---
+++ Neighborhood.py
@@ -0,0 +1,7 @@
+class Neighborhood:
+    def __init__(self, neighborhoodType, dimension):
+        self.neighborhoodType = NeighborhoodType
+        self.dimension = dimension
+
+    def getBestNeighbor(self):
+        return [0]
